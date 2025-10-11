3977749d87a2ffc28d28df40d11a901df051bc71
WebSystemsG9ProjectWork <websystemsg9project@gmail.com>
2025-10-10 21:32:43 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
-import Navbar from './pages/Navbar';
-import { ToastProvider } from './context/ToastContext';
+import { HashRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import Navbar from "./pages/Navbar";
+import { ToastProvider } from "./context/ToastContext";
 import SignIn from "./pages/SignIn";
-import './styles/gradient.css';
+import "./styles/gradient.css";
 
 function App() {
   return (
     <ToastProvider>
-        <Router>
-         <Navbar/>
-          <Routes>
-            <Route path="/" element={<Navigate to="/Home.html" replace />} />
-            <Route path="/signin" element={<SignIn />} />
-          </Routes>
-        </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* keep root redirect if you want, but point to index route in SPA */}
+          <Route path="/" element={<Navigate to="/" replace />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </Router>
     </ToastProvider>
   );
 }
